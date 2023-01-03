320f18d3f640d6f28b8968778de098b49feaf2b0
Olivier Guyot <olivier.guyot@camptocamp.com>
2023-01-03 08:49:36 +0100

    fix dev workflow, use config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,16 +1,29 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module, OnApplicationBootstrap } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { join } from 'path';
+import configuration from './configuration';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, 'client'),
     }),
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationBootstrap {
+  logger = new Logger(AppModule.name);
+
+  constructor(private config: ConfigService) {}
+
+  onApplicationBootstrap() {
+    this.logger.log(`\x1b[37mOpen http://localhost:${this.config.get('PORT')}`);
+  }
+}
